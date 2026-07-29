13c3a0730ecd62e55777df7a3034263755874cf7
Rawad Naameh <rawad.naameh@mobileartsme.com>
2026-07-29 16:15:15 +0300

    repair .env file

--- frontend/features/campaign-editor/api/apiConfig.ts
+++ frontend/features/campaign-editor/api/apiConfig.ts
@@ -1,3 +1,1 @@
-// frontend/features/campaign-editor/api/apiConfig.ts
-
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || '';
